c0408374363aeed6ffa1decb16de26a4b382cf86
Ethan Holz <iplaybassx2@gmail.com>
2023-06-26 11:05:54 -0500

    ci: added the ability to build via the flake

--- flake.nix
+++ flake.nix
@@ -7,9 +7,11 @@
   outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        name = "git-cc";
         pkgs = (import nixpkgs) {
           inherit system;
         };
+        buildGoModule = pkgs.buildGo119Module;
       in
       rec {
         devShell = pkgs.mkShell {
@@ -29,6 +31,19 @@
             vhs
             bashInteractive
           ];
+        };
+        packages = rec {
+          default = git-cc;
+          git-cc = buildGoModule {
+            src = ./.;
+            pname = name;
+            version = "0.0.17";
+            vendorSha256 = "sha256-by2OO4HmUFYPelwA1mdD+7y8sK+C8Ga6gIL6uxB+V8k=";
+            meta = {
+              description = "git-cc is a tool to help you write better commit messages";
+              homepage = "git@github.com:ethanholz/git-cc.git";
+            };
+          };
         };
       }
     );
